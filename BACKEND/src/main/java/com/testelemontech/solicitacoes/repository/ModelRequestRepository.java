74797f2bd737d96d4889830e42c059633ca49a4d
litch <matheushyago92@gmail.com>
2025-03-24 18:07:55 -0300

    ajuste
    Signed-off-by: litch <matheushyago92@gmail.com>

--- BACKEND/src/main/java/com/testelemontech/solicitacoes/repository/ModelRequestRepository.java
+++ BACKEND/src/main/java/com/testelemontech/solicitacoes/repository/ModelRequestRepository.java
@@ -4,6 +4,5 @@ import com.testelemontech.solicitacoes.model.ModelRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ModelRequestRepository extends JpaRepository<ModelRequest, Long> {
-
     ModelRequest findByCodigoSolicitacao(String codigoSolicitacao);
 }
